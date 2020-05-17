3857059c77ee12e98418f71a01122c91e24180bd
Administrator <Administrator@192.168.1.237>
2020-05-17 12:19:12 +0800

    每秒生产数量

--- src/main/java/com/aebiz/controller/ProducerController.java
+++ src/main/java/com/aebiz/controller/ProducerController.java
@@ -1,11 +1,13 @@
 package com.aebiz.controller;
 
+import java.util.UUID;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,23 +32,22 @@ public class ProducerController {
 	 */
 	@RequestMapping("/addOneMsg")
 //	@Transactional
-	public String addOneMsg(@RequestParam(value="topicName", required=false) String topicName
+	public String addOneMsg(@RequestParam(value="topicName") String topicName
 			, @RequestParam(value="keyName", required=false) String keyName
 			, @RequestParam(value="msg", required=false) String msg
 			) {
 		if(StringUtils.isBlank(topicName)) {
 			return "主题是必输项";
 		}
-		if(StringUtils.isBlank(keyName)) {
-			return "消息KEY是必输项";
-		}
 		
 		long t1 = System.currentTimeMillis();
-		//生成一条消息，将就着用
-		String oneMsg = GeneDataUtil.geneOneMessage();
-		//不行，不将就。就要用自己的
-		if(StringUtils.isNotBlank(msg)) {
-			oneMsg = msg;
+		
+		//前台没有，后台补之
+		if(StringUtils.isBlank(msg)) {
+			msg = GeneDataUtil.geneOneMessage();
+		}
+		if(StringUtils.isBlank(keyName)) {
+			keyName = UUID.randomUUID().toString();
 		}
 		
 		KafkaTemplate<String, String> template = kafkaTemplateConfig.getKafkaTemplate();
@@ -55,22 +56,23 @@ public class ProducerController {
 				template.send(topicName,
 //						null,
 //						System.currentTimeMillis() + 100000000l, //测试时间戳
-						keyName, oneMsg);
+						keyName, msg);
 
 		try {
 			//异步，1ms
-//			future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
-//				@Override
-//				public void onSuccess(SendResult<String, String> result) {
-//					long t2 = System.currentTimeMillis();
-//					System.out.println("成功！" + (t2 -t1) + "ms");
-//				}
-//				@Override
-//				public void onFailure(Throwable ex) {
-//					long t2 = System.currentTimeMillis();
-//					System.out.println("失败！" + (t2 -t1) + "ms" + "，错误原因为" + ex.getMessage());
-//				}
-//			});
+			future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+				@Override
+				public void onSuccess(SendResult<String, String> result) {
+					long t2 = System.currentTimeMillis();
+				}
+				@Override
+				public void onFailure(Throwable ex) {
+					long t2 = System.currentTimeMillis();
+					
+					String msg = "失败！" + (t2 -t1) + "ms" + "，错误原因为" + ex.getMessage();
+					log.error(msg);
+				}
+			});
 			
 			//同步，10ms，比异步慢10倍
 //			SendResult<String, String> ret = future.get(10, TimeUnit.SECONDS);
@@ -88,27 +90,16 @@ public class ProducerController {
 	}
 	
 	/**
-	 * 向指定topic生产无数条消息，可以设置毫秒间隔
-	 * http://localhost:9201/producer/addNMsgs?topicName=topic_1p_1r&keyName=key1&period=5000
+	 * 向指定topic生产N条消息
+	 * http://localhost:9201/producer/addNMsgs?topicName=topic_1p_1r&num=1
 	 */
 	@RequestMapping("/addNMsgs")
 	public String addNMsgs(@RequestParam("topicName") String topicName
-			, @RequestParam("keyName") String keyName
-			, @RequestParam("period") long period) {
-		int count = 0;
-		while(true) {
-			try {
-				//生产一条消息
-				this.addOneMsg(topicName, keyName, null);
-				count++;
-				
-				log.debug("第[" + count + "]个消息发送成功！");
-				
-				Thread.sleep(period);
-			} catch(Exception e) {
-				log.debug("第[" + count + "]个消息发送失败：" + e.getMessage());
-			}
+			, @RequestParam("num") int num) {
+		for(int i = 0; i < num; i++) {
+			this.addOneMsg(topicName, null, null);
 		}
+		return "SUCCESS";
 	}
 	
 
